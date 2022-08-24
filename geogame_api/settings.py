f0a8553db1541c642c826262ead4d5ec7cfc7775
DyogoBendo <dyogoromagnabendo@gmail.com>
2022-08-23 22:31:32 -0300

    returning cors

--- geogame_api/settings.py
+++ geogame_api/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,6 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+]
 
 django_heroku.settings(locals())
 
